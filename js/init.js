8f8222ac75ac3abffd97d32844cdc06032874e1d
Tim <timothy.w.holman@gmail.com>
2014-07-08 08:57:45 -0400

    base extension scaffold

---
+++ js/init.js
@@ -0,0 +1,14 @@
+/* 
+ *  America F Yeah
+ *  - Initializer, injects the Americanizer script on new pages
+ */
+
+chrome.tabs.onUpdated.addListener(function(id, info, tab){
+
+    if (tab.status !== "complete"){
+        return;
+    }
+
+    chrome.tabs.executeScript(null, {"file": "./js/americanize.js"});
+
+});
