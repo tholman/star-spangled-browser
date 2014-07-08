a632642e5c7e4275579f4ea6cf5a254faaed5743
Tim <timothy.w.holman@gmail.com>
2014-07-08 09:15:23 -0400

    text finding, and replacement

--- js/americanize.js
+++ js/americanize.js
@@ -1,1 +1,27 @@
-console.log( "what?" );
+
+
+// http://stackoverflow.com/questions/1774846/how-to-search-replace-text-with-an-a-href-wrapper-in-javascript
+function findPlainText(element, substring, callback) {
+    for (var childi= element.childNodes.length; childi-->0;) {
+        var child= element.childNodes[childi];
+        if (child.nodeType===1) {
+            findPlainText(child, substring, callback);
+        } else if (child.nodeType===3) {
+            var index = child.data.length;
+            while (true) {
+                index = child.data.lastIndexOf(substring, index);
+                if (index===-1)
+                    break;
+                callback.call(window, child, index)
+            }
+        }
+    }
+}
+
+var substring= 'tral';
+findPlainText(document.body, substring, function(node, index) {
+    node.splitText(index+substring.length);
+    var span= document.createElement('span');
+    span.appendChild(node.splitText(index));
+    node.parentNode.insertBefore(span, node.nextSibling);
+});
